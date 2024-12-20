cd83dfdadf792d253c9f32ad337ca5e575e5cd65
OlgerQuispe <goldsag43@gmail.com>
2024-12-20 15:15:00 -0500

    added buttons likes and views

---
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/videos_post.dart';
+
+class VideoButtons extends StatelessWidget {
+
+  final VideoPost video;
+
+  const VideoButtons({super.key, required this.video});
+
+  @override
+  Widget build(BuildContext context) {
+    //Botones encima de otros 
+    return Column(
+      children: [
+        _CustomIconButton(value: video.likes, iconColor: Colors.red, iconData: Icons.favorite,),
+        _CustomIconButton(value: video.views, iconData: Icons.favorite,)
+      ],
+    );
+  }
+}
+
+class _CustomIconButton extends StatelessWidget {
+  final int value;
+  final IconData iconData;
+  final Color color;
+  const _CustomIconButton({
+    required this.value, 
+    required this.iconData, 
+    iconColor
+  }): color = iconColor ?? Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+          onPressed: () {}, 
+          icon: Icon(iconData, color: color, size: 30,)),
+
+        Text('$value'),
+      ],
+    );
+  }
+}
--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/videos_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/shared/data/local_video_post.dart';
 
 class VideoScrollableView extends StatelessWidget {
   const VideoScrollableView({super.key, required this.videos});
@@ -10,17 +12,26 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical, //Direccion del scroll
       physics: const BouncingScrollPhysics(), //Fisica de rebote del scroll
-      children: [
-        Container( color: Colors.red,),
-        Container(color: Colors.blue,),
-        Container(color: Colors.teal,),
-        Container(color: Colors.yellow,),
-        Container(color: Colors.pink,),
-        Container(color: Colors.deepPurple,),
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+            //videoplayer + gradiente
+
+
+            //botones
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost)
+            ),
+          ],
+        );
+      },
     );
   }
 }
