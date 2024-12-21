336265b098bd2a3b92a6d94c4dc0de9dda3c55b4
OlgerQuispe <goldsag43@gmail.com>
2024-12-20 21:31:49 -0500

    controller of video, FutureBuilder

--- lib/presentation/widgets/shared/video/fullscreen_player.dart
+++ lib/presentation/widgets/shared/video/fullscreen_player.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class FullScreenPlayer extends StatelessWidget {
-
+class FullScreenPlayer extends StatefulWidget {
   final String videoUrl;
   final String caption;
 
+  const FullScreenPlayer(
+      {super.key, required this.videoUrl, required this.caption});
 
-  const FullScreenPlayer({
-    super.key, 
-    required this.videoUrl, 
-    required this.caption});
+  @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+  late VideoPlayerController controller;
+  //Inicializar el video
+  @override
+  void initState() {
+    super.initState();
+
+    controller = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
+  }
+
+  //Evitar que el video se siga reproduciendo en segundo plano
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    //FutureBuilder para esperar a que el video se inicialice y construir el widget
+    return FutureBuilder(
+        future: controller.initialize(),
+        builder: (context, snapshot) {
+          return const Center(child: CircularProgressIndicator(strokeWidth: 2));
+        });
   }
 }
