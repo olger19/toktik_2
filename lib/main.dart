46f160390d5e49e65aaf53fa5e75b7b167718749
OlgerQuispe <goldsag43@gmail.com>
2024-12-24 17:33:47 -0500

    using repository and origin of data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infrastructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 import 'package:toktik/presentation/screens/discover/providers/discover_provider.dart';
 
@@ -11,12 +14,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+
+    final videoPostsReposity = VideoPostsRepositoryImpl(
+      videoPostsDatasource: LocalVideoDatasource()
+    );
+
+
     return MultiProvider(
       providers: [
         //Instacia para tener accesible la clase DiscoverProvider a toda la aplicacion
         ChangeNotifierProvider(
           lazy: false, // 
-          create: (_) => DiscoverProvider()..loadNextPage() 
+          create: (_) => DiscoverProvider( videoPostsRepositoy: videoPostsReposity )..loadNextPage() 
           ), // Llamamos al metodo loadNextPage con cascada
       ],
       child: MaterialApp(
--- lib/presentation/screens/discover/providers/discover_provider.dart
+++ lib/presentation/screens/discover/providers/discover_provider.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/videos_post.dart';
-import 'package:toktik/infrastructure/models/local_video_model.dart';
-import 'package:toktik/shared/data/local_video_post.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
+
 
 
 // Ahora tengo accesible esta clase a lo largo de toda mi aplicacion
 class DiscoverProvider extends ChangeNotifier{
-  // TODO: DataSources, Repositories
   // Repository Va ser quien me va a permitir a mi lanzar las peticiones respectivas, DataSource Fuente de la data
+
+  final VideoPostsRepository videoPostsRepositoy;
 
   bool initialLoading = true;
 
   // Tener una lista de VideoPost
   List<VideoPost> videos = [];
 
+  DiscoverProvider({
+    required this.videoPostsRepositoy
+    });
+
 
   
   Future<void> loadNextPage()async {
     //Podemos comentar el await para evitar el delay de los videos
     //await Future.delayed(const Duration(seconds: 2));
-    final List<VideoPost> newVideos = videoPosts.map( (video) => LocalVideoModel.fromJson(video).toVideoPostEntity()).toList();
-    
+    //final List<VideoPost> newVideos = videoPosts.map( (video) => LocalVideoModel.fromJson(video).toVideoPostEntity()).toList();
+    final newVideos = await videoPostsRepositoy.getTrendingVideoByPage(1);
+
     videos.addAll(newVideos);
     initialLoading = false;
     notifyListeners();
